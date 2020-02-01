78766d558e79a94cf514eb6880242a9f44dc3930
Ken H <kenthecoder@outlook.com>
2020-02-01 17:45:32 -0500

    Added server-side logic to undo ShareKnowledge events.

---
+++ serverCode/actionPages/undoShareKnowledge.php
@@ -0,0 +1,59 @@
+<?php
+    try
+    {
+        session_start();
+        
+        if (!isset($_SESSION["game"]))
+            throw new Exception("Game not found.");
+
+        if (!isset($_POST["currentStep"]))
+            throw new Exception("Current step not set.");
+        
+        if (!isset($_POST["activeRole"]))
+            throw new Exception("Role not set.");
+        
+        if (!isset($_POST["eventID"]))
+            throw new Exception("Event id not set.");
+        
+        $game = $_SESSION["game"];
+        $currentStep = $_POST["currentStep"];
+        $activeRole = $_POST["activeRole"];
+        $eventID = $_POST["eventID"];
+        
+        require "../connect.php";
+        include "../utilities.php";
+
+        $event = getEventById($mysqli, $game, $eventID);
+        validateEventCanBeUndone($mysqli, $game, $event);
+
+        $role = $event["role"];
+        $eventType = $event["eventType"];
+        $eventDetails = explode(",", $event["details"]);
+        $cardKey = $eventDetails[0];
+        $giverRole = $eventDetails[1];
+        $receiverRole = $eventDetails[2];
+
+        $mysqli->autocommit(FALSE);
+
+        moveCardsToPile($mysqli, $game, "player", $receiverRole, $giverRole, $cardKey);
+        
+        $response["undoneEventIds"] = array($eventID);
+        $response["prevStepName"] = previousStep($mysqli, $game, $activeRole, $currentStep);
+        deleteEvent($mysqli, $game, $eventID);
+    }
+    catch(Exception $e)
+    {
+        $response["failure"] = "Failed to undo Share Knowledge: " . $e->getMessage();
+    }
+    finally
+    {
+        if (isset($response["failure"]))
+            $mysqli->rollback();
+        else
+            $mysqli->commit();
+        
+        $mysqli->close();
+
+        echo json_encode($response);
+    }
+?>
