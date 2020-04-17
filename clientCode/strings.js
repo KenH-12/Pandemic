9c7d288d6cfe1a8c5a7de50f4647a60c0d959fea
Ken H <kenthecoder@outlook.com>
2020-04-17 10:53:16 -0400

    Fixed a typo.

--- clientCode/strings.js
+++ clientCode/strings.js
@@ -12,7 +12,7 @@ strings = {
     victoryCondition: "<span class='warning'>🏆</span>Discover cures to all 4 diseases and your team wins immediately, no matter how many cubes are on the board.",
 
     contingencyPlannerCardText: `<li><span>As an action, take any discarded Event card and store it.</span></li><li><span>When you play the stored Event card, remove it from the game.</span></li><li><span>Limit: 1 stored Event card at a time, which is not part of your hand.</span></li>`,
-    dispatcherCardText: `<li><span>Move another role''s pawn as if it were yours.</span></li><li><span>As an action, move any pawn to a city with another pawn.</span></li>`,
+    dispatcherCardText: `<li><span>Move another role's pawn as if it were yours.</span></li><li><span>As an action, move any pawn to a city with another pawn.</span></li>`,
     medicCardText: `<li><span>Remove all cubes of one color when doing Treat Disease.</span></li><li><span>Automatically remove cubes of cured diseases from the city you are in (and prevent them from being placed there).</span></li>`,
     operationsExpertCardText: `<li><span>As an action, build a research station in the city you are in (no City card needed).</span></li><li><span>Once per turn as an action, move from a research station to any city by discarding any City card.</span></li>`,
     quarantineSpecialistCardText: `<li><span>Prevent disease cube placements (and outbreaks) in the city you are in and all cities connected to it.</span></li>`,
