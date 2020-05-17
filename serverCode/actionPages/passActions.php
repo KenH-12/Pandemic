94b5fb524f647265caf490ebf216fed3f59f2de1
Ken H <kenthecoder@outlook.com>
2020-05-17 19:20:53 -0400

    The server-side code for the Pass action now uses PDO instead of mysqli.

--- serverCode/actionPages/passActions.php
+++ serverCode/actionPages/passActions.php
@@ -2,47 +2,55 @@
     try
     {
         session_start();
-        require "../connect.php";
-        include "../utilities.php";
         
         if (!isset($_SESSION["game"]))
             throw new Exception("Game not found.");
 
-        if (!isset($_POST["role"]))
+        require "../connect.php";
+        require "../utilities.php";
+
+        $details = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($details["role"]))
             throw new Exception("Role not set.");
         
-        if (!isset($_POST["currentStep"]))
+        if (!isset($details["currentStep"]))
             throw new Exception("Current step not set.");
         
         $game = $_SESSION["game"];
-        $role = $_POST["role"];
-        $currentStep = $_POST["currentStep"];
+        $role = $details["role"];
+        $currentStep = $details["currentStep"];
 
         // Record the number of actions forfeited as this information will be displayed in the event history.
         $MAX_NUM_ACTIONS = 4;
-        $eventDetails = $MAX_NUM_ACTIONS - countActionsTakenThisTurn($mysqli, $game);
+        $eventDetails = $MAX_NUM_ACTIONS - countEventsOfTurn($pdo, $game, getActionEventTypes());
 
-        $mysqli->autocommit(FALSE);
+        $pdo->beginTransaction();
         
         $EVENT_TYPE = "pa";
-        $response["events"] = recordEvent($mysqli, $game, $EVENT_TYPE, "$eventDetails", $role);
+        $response["events"] = recordEvent($pdo, $game, $EVENT_TYPE, "$eventDetails", $role);
 
         $NEXT_STEP = "draw";
-        $response["nextStep"] = updateStep($mysqli, $game, $currentStep, $NEXT_STEP, $role);
+        $response["nextStep"] = updateStep($pdo, $game, $currentStep, $NEXT_STEP, $role);
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Failed to pass remaining actions: PDOException: " . $e->getMessage();
     }
     catch(Exception $e)
     {
-        $response["failure"] = $e->getMessage();
+        $response["failure"] = "Failed to pass remaining actions: " . $e->getMessage();
     }
     finally
     {
-        if (isset($response["failure"]))
-            $mysqli->rollback();
-        else
-            $mysqli->commit();
+        if ($pdo->inTransaction())
+        {
+            if (isset($response["failure"]))
+                $pdo->rollback();
+            else
+                $pdo->commit();
+        }
         
-        $mysqli->close();
-
         echo json_encode($response);
     }
 ?>
