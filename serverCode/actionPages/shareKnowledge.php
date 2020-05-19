44e34dbfdf83c793384fd12c39b389eff65371a7
Ken H <kenthecoder@outlook.com>
2020-05-18 20:39:27 -0400

    The server-side code for the Share Knowledge action now uses PDO instead of mysqli.

--- serverCode/actionPages/shareKnowledge.php
+++ serverCode/actionPages/shareKnowledge.php
@@ -2,87 +2,97 @@
     try
     {
         session_start();
-        require "../connect.php";
-        include "../utilities.php";
         
         if (!isset($_SESSION["game"]))
             throw new Exception("Game not found.");
+        
+        require "../connect.php";
+        require "../utilities.php";
 
-        if (!isset($_POST["actionCode"]))
+        $details = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($details["actionCode"]))
             throw new Exception("Action code not set.");
             
-        if (!isset($_POST["currentStep"]))
+        if (!isset($details["currentStep"]))
             throw new Exception("Current step not set.");
         
-        if (!isset($_POST["role"]))
+        if (!isset($details["role"]))
             throw new Exception("Role not set.");
            
-        if (!isset($_POST["giver"]))
+        if (!isset($details["giver"]))
             throw new Exception("Giver not set.");
          
-        if (!isset($_POST["receiver"]))
+        if (!isset($details["receiver"]))
             throw new Exception("Receiver not set.");
         
-        if (!isset($_POST["cardKey"]))
+        if (!isset($details["cardKey"]))
             throw new Exception("Card key not set.");
         
         $game = $_SESSION["game"];
-        $eventType = $_POST["actionCode"];
-        $currentStep = $_POST["currentStep"];
-        $role = $_POST["role"];
-        $giver = $_POST["giver"];
-        $receiver = $_POST["receiver"];
-        $cardKey = $_POST["cardKey"];
+        $eventType = $details["actionCode"];
+        $currentStep = $details["currentStep"];
+        $role = $details["role"];
+        $giver = $details["giver"];
+        $receiver = $details["receiver"];
+        $cardKey = $details["cardKey"];
 
-        $mysqli->autocommit(FALSE);
-
-        $playerLocations = $mysqli->query("SELECT location
-                                            FROM vw_player
-                                            WHERE game = $game
-                                            AND rID IN ($giver, $receiver)");
+        $stmt = $pdo->prepare("SELECT location
+                                FROM vw_player
+                                WHERE game = ?
+                                AND rID IN (?, ?)");
+        $stmt->execute([$game, $giver, $receiver]);
+        $playerLocations = $stmt->fetchAll();
         // Confirm that both players are in the same city.
         $cityKey = false;
-        while ($row = mysqli_fetch_assoc($playerLocations))
+        foreach ($playerLocations as $row)
 		{
             if (!$cityKey)
                 $cityKey = $row["location"];
-            else if ($row["location"] != $cityKey)
+            else if ($row["location"] !== $cityKey)
                 throw new Exception("Share Knowledge requires both players to be in the same city.");
 		}
 
         // If the cardKey does not match the cityKey,
         // the giver must be the Researcher (see Researcher special ability).
-        if ($cardKey != $cityKey
-            && getRoleName($mysqli, $giver) != "Researcher")
-            throw new Exception("Invalid 'Share Knowledge' attempt: Only the Researcher can give a card that does not match their current location.");
+        if ($cardKey !== $cityKey
+            && getRoleName($pdo, $giver) !== "Researcher")
+            throw new Exception("Only the Researcher can give a card that does not match their current location.");
 
+        $pdo->beginTransaction();
+        
         $cardType = "player";
-        moveCardsToPile($mysqli, $game, $cardType, $giver, $receiver, $cardKey);
+        moveCardsToPile($pdo, $game, $cardType, $giver, $receiver, $cardKey);
         
         $eventDetails = "$cardKey,$giver,$receiver";
-        $response["events"] = recordEvent($mysqli, $game, $eventType, $eventDetails, $role);
+        $response["events"] = recordEvent($pdo, $game, $eventType, $eventDetails, $role);
 
-        if (roleHasTooManyCards($mysqli, $game, $receiver))
+        if (roleHasTooManyCards($pdo, $game, $receiver))
         {
             $nextStep = "hand limit";
-            $response["nextStep"] = updateStep($mysqli, $game, $currentStep, $nextStep, $role);
+            $response["nextStep"] = updateStep($pdo, $game, $currentStep, $nextStep, $role);
         }
         else
-            $response["nextStep"] = nextStep($mysqli, $game, $currentStep, $role);
+            $response["nextStep"] = nextStep($pdo, $game, $currentStep, $role);
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Share Knowledge failed: PDOException:" . $e->getMessage();
     }
     catch(Exception $e)
     {
-        $response["failure"] = $e->getMessage();
+        $response["failure"] = "Share Knowledge failed: " . $e->getMessage();
     }
     finally
     {
-        if (isset($response["failure"]))
-            $mysqli->rollback();
-        else
-            $mysqli->commit();
+        if ($pdo->inTransaction())
+        {
+            if (isset($response["failure"]))
+                $pdo->rollback();
+            else
+                $pdo->commit();
+        }
         
-        $mysqli->close();
-
         echo json_encode($response);
     }
 ?>
