d41b393a2985e4ea21d3c50e3bee535db6c951e2
Ken H <kenthecoder@outlook.com>
2020-05-21 19:16:44 -0400

    The server-side code for undoing Build Research Station or Government Grant now uses PDO instead of mysqli.

--- serverCode/actionPages/undoPages/undoResearchStationPlacement.php
+++ serverCode/actionPages/undoPages/undoResearchStationPlacement.php
@@ -2,72 +2,81 @@
     try
     {
         session_start();
-        require "../../connect.php";
-        include "../../utilities.php";
         
         if (!isset($_SESSION["game"]))
             throw new Exception("Game not found.");
 
-        if (!isset($_POST["currentStep"]))
+        $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
+        require "$rootDir/Pandemic/serverCode/connect.php";
+        require "$rootDir/Pandemic/serverCode/utilities.php";
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data["currentStep"]))
             throw new Exception("Current step not set.");
         
-        if (!isset($_POST["activeRole"]))
+        if (!isset($data["activeRole"]))
             throw new Exception("Role not set.");
         
-        if (!isset($_POST["eventID"]))
+        if (!isset($data["eventID"]))
             throw new Exception("Event id not set.");
         
         $game = $_SESSION["game"];
-        $currentStep = $_POST["currentStep"];
-        $activeRole = $_POST["activeRole"];
-        $eventID = $_POST["eventID"];
+        $currentStep = $data["currentStep"];
+        $activeRole = $data["activeRole"];
+        $eventID = $data["eventID"];
 
         $BUILD_RESEARCH_STATION = "rs";
         $GOVERNMENT_GRANT = "gg";
 
-        $event = getEventById($mysqli, $game, $eventID);
-        validateEventCanBeUndone($mysqli, $game, $event);
+        $event = getEventById($pdo, $game, $eventID);
+        validateEventCanBeUndone($pdo, $game, $event);
 
         $role = $event["role"];
         $eventType = $event["eventType"];
         $eventDetails = explode(",", $event["details"]);
         $cityKey = $eventDetails[0];
+        $relocationKey = isset($eventDetails[1]) ? $eventDetails[1] : "0";
 
-        $mysqli->autocommit(FALSE);
+        $pdo->beginTransaction();
 
         if ($eventType === $BUILD_RESEARCH_STATION)
         {
             // (the Operations Expert can perform Build Research Station for free).
-            if (getRoleName($mysqli, $role) !== "Operations Expert")
-                moveCardsToPile($mysqli, $game, "player", "discard", $role, $cityKey);
+            if (getRoleName($pdo, $role) !== "Operations Expert")
+                moveCardsToPile($pdo, $game, "player", "discard", $role, $cityKey);
 
-            $response["prevStepName"] = previousStep($mysqli, $game, $activeRole, $currentStep);
+            $response["prevStepName"] = previousStep($pdo, $game, $activeRole, $currentStep);
         }
         else if ($eventType === $GOVERNMENT_GRANT)
         {
             $GOVERNMENT_GRANT_CARDKEY = "gove";
-            $response["wasContingencyCard"] = moveEventCardToPrevPile($mysqli, $game, $GOVERNMENT_GRANT_CARDKEY, $event);
+            $response["wasContingencyCard"] = moveEventCardToPrevPile($pdo, $game, $GOVERNMENT_GRANT_CARDKEY, $event);
 
-            if (roleHasTooManyCards($mysqli, $game, $role))
+            if (roleHasTooManyCards($pdo, $game, $role))
             {
-                $prevStep = getPreviousDiscardStepName($mysqli, $game);
-                $response["prevStepName"] = updateStep($mysqli, $game, $currentStep, $prevStep, $activeRole);
+                $prevStep = getPreviousDiscardStepName($pdo, $game);
+                $response["prevStepName"] = updateStep($pdo, $game, $currentStep, $prevStep, $activeRole);
             }
         }
         else
             throw new Exception("Invalid event type: '$eventType'");
 
         // If the action relocated a research station, place it back on the original city.
-        if (isset($eventDetails[1]))
-        {
-            $relocationKey = $eventDetails[1];
-            placeResearchStation($mysqli, $game, $relocationKey, $cityKey);
-        }
+        if ($relocationKey)
+            placeResearchStation($pdo, $game, $relocationKey, $cityKey);
         else
-            removeResearchStation($mysqli, $game, $cityKey);
+        {
+            $remove = true;
+            addOrRemoveResearchStation($pdo, $game, $cityKey, $remove);
+        }
 
         $response["undoneEventIds"] = array($eventID);
-        deleteEvent($mysqli, $game, $eventID);
+        deleteEvent($pdo, $game, $eventID);
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Failed to undo research station placement: PDOException: " . $e->getMessage();
     }
     catch(Exception $e)
     {
@@ -75,13 +84,14 @@
     }
     finally
     {
-        if (isset($response["failure"]))
-            $mysqli->rollback();
-        else
-            $mysqli->commit();
+        if ($pdo->inTransaction())
+        {
+            if (isset($response["failure"]))
+                $pdo->rollback();
+            else
+                $pdo->commit();
+        }
         
-        $mysqli->close();
-
         echo json_encode($response);
     }
 ?>
