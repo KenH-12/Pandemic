28f408b4a00bbd5ec7d0afbf1384e53a241b3198
Ken H <kenthecoder@outlook.com>
2020-05-21 19:05:15 -0400

    The server-side code for undoing Plan Contingency now uses PDO instead of mysqli.

--- serverCode/actionPages/undoPages/undoPlanContingency.php
+++ serverCode/actionPages/undoPages/undoPlanContingency.php
@@ -2,28 +2,32 @@
     try
     {
         session_start();
-        require "../../connect.php";
-        include "../../utilities.php";
         
         if (!isset($_SESSION["game"]))
             throw new Exception("Game not found.");
 
-        if (!isset($_POST["currentStep"]))
+        $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
+        require "$rootDir/Pandemic/serverCode/connect.php";
+        require "$rootDir/Pandemic/serverCode/utilities.php";
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data["currentStep"]))
             throw new Exception("Current step not set.");
         
-        if (!isset($_POST["activeRole"]))
+        if (!isset($data["activeRole"]))
             throw new Exception("Role not set.");
         
-        if (!isset($_POST["eventID"]))
+        if (!isset($data["eventID"]))
             throw new Exception("Event id not set.");
         
         $game = $_SESSION["game"];
-        $currentStep = $_POST["currentStep"];
-        $activeRole = $_POST["activeRole"];
-        $eventID = $_POST["eventID"];
+        $currentStep = $data["currentStep"];
+        $activeRole = $data["activeRole"];
+        $eventID = $data["eventID"];
 
-        $event = getEventById($mysqli, $game, $eventID);
-        validateEventCanBeUndone($mysqli, $game, $event);
+        $event = getEventById($pdo, $game, $eventID);
+        validateEventCanBeUndone($pdo, $game, $event);
 
         $role = $event["role"];
         $cardKey = $event["details"];
@@ -31,13 +35,17 @@
         $currentPile = "contingency";
         $newPile = "discard";
 
-        $mysqli->autocommit(FALSE);
+        $pdo->beginTransaction();
 
-        moveCardsToPile($mysqli, $game, $cardType, $currentPile, $newPile, $cardKey);
-        $response["prevStepName"] = previousStep($mysqli, $game, $activeRole, $currentStep);
+        moveCardsToPile($pdo, $game, $cardType, $currentPile, $newPile, $cardKey);
+        $response["prevStepName"] = previousStep($pdo, $game, $activeRole, $currentStep);
         
         $response["undoneEventIds"] = array($eventID);
-        deleteEvent($mysqli, $game, $eventID);
+        deleteEvent($pdo, $game, $eventID);
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Failed to undo Plan Contingency: PDOException: " . $e->getMessage();
     }
     catch(Exception $e)
     {
@@ -45,13 +53,14 @@
     }
     finally
     {
-        if (isset($response["failure"]))
-            $mysqli->rollback();
-        else
-            $mysqli->commit();
+        if ($pdo->inTransaction())
+        {
+            if (isset($response["failure"]))
+                $pdo->rollback();
+            else
+                $pdo->commit();
+        }
         
-        $mysqli->close();
-
         echo json_encode($response);
     }
 ?>
