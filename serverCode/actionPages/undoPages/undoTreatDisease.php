c9bd1688c7b82960db45b1701a56a809d1ab4a5b
Ken H <kenthecoder@outlook.com>
2020-05-22 16:10:33 -0400

    The server-side code for undoing Treat Disease events now uses PDO instead of mysqli.

--- serverCode/actionPages/undoPages/undoTreatDisease.php
+++ serverCode/actionPages/undoPages/undoTreatDisease.php
@@ -2,28 +2,32 @@
     try
     {
         session_start();
-        require "../../connect.php";
-        include "../../utilities.php";
         
         if (!isset($_SESSION["game"]))
             throw new Exception("Game not found.");
 
-        if (!isset($_POST["currentStep"]))
+        $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
+        require "$rootDir/Pandemic/serverCode/connect.php";
+        require "$rootDir/Pandemic/serverCode/utilities.php";
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data["currentStep"]))
             throw new Exception("Current step not set.");
         
-        if (!isset($_POST["activeRole"]))
+        if (!isset($data["activeRole"]))
             throw new Exception("Role not set.");
         
-        if (!isset($_POST["eventID"]))
+        if (!isset($data["eventID"]))
             throw new Exception("Event id not set.");
         
         $game = $_SESSION["game"];
-        $currentStep = $_POST["currentStep"];
-        $activeRole = $_POST["activeRole"];
-        $eventID = $_POST["eventID"];
+        $currentStep = $data["currentStep"];
+        $activeRole = $data["activeRole"];
+        $eventID = $data["eventID"];
 
-        $event = getEventById($mysqli, $game, $eventID);
-        validateEventCanBeUndone($mysqli, $game, $event);
+        $event = getEventById($pdo, $game, $eventID);
+        validateEventCanBeUndone($pdo, $game, $event);
 
         $role = $event["role"];
         $eventDetails = explode(",", $event["details"]);
@@ -31,17 +35,21 @@
         $diseaseColor = $eventDetails[1];
         $numCubesRemoved = $eventDetails[2] - $eventDetails[3];
 
-        $mysqli->autocommit(FALSE);
+        $pdo->beginTransaction();
         
-        addCubesToCity($mysqli, $game, $cityKey, $diseaseColor, $numCubesRemoved);
+        addCubesToCity($pdo, $game, $cityKey, $diseaseColor, $numCubesRemoved);
         
         $response["undoneEventIds"] = array($eventID);
         // Undo any eradication events that were triggered by the Treat Disease event.
-        if ($triggeredEventIds = undoEventsTriggeredByEvent($mysqli, $game, $eventID))
+        if ($triggeredEventIds = undoEventsTriggeredByEvent($pdo, $game, $eventID))
             $response["undoneEventIds"] = array_merge($response["undoneEventIds"], $triggeredEventIds);
 
-        deleteEvent($mysqli, $game, $eventID);
-        $response["prevStepName"] = previousStep($mysqli, $game, $activeRole, $currentStep);
+        deleteEvent($pdo, $game, $eventID);
+        $response["prevStepName"] = previousStep($pdo, $game, $activeRole, $currentStep);
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Failed to undo Treat Disease: PDOException: " . $e->getMessage();
     }
     catch(Exception $e)
     {
@@ -49,13 +57,14 @@
     }
     finally
     {
-        if (isset($response["failure"]))
-            $mysqli->rollback();
-        else
-            $mysqli->commit();
+        if ($pdo->inTransaction())
+        {
+            if (isset($response["failure"]))
+                $pdo->rollback();
+            else
+                $pdo->commit();
+        }
         
-        $mysqli->close();
-
         echo json_encode($response);
     }
 ?>
