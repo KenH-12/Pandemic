89fbcc67e8fcc2e5b7599c42c4200a0f7ff58c70
Ken H <kenthecoder@outlook.com>
2020-06-01 14:13:15 -0400

    Fixed main menu style. Removed the 'randomize roles' checkbox until time is found to add role selection functionality.

--- mainMenu.php
+++ mainMenu.php
@@ -1,26 +1,27 @@
 <?php
     $mainMenu = "<div class='divInputControl'>
-                    <label for='radDifficulty'>Select Difficulty:</label>
-                    <br />
-                    <input type='radio' name='radDifficulty' value='1' checked='checked'>Introductory</option>
-                    <br />
-                    <input type='radio' name='radDifficulty' value='2'>Normal</option>
-                    <br />
-                    <input type='radio' name='radDifficulty' value='3'>Heroic</option>
+                    <label for='radDifficulty' class='subtitle'>Select Difficulty:</label>
+                    <div>
+                        <input type='radio' name='radDifficulty' id='radIntroductory' value='4' checked='checked'>
+                        <label for='radIntroductory'>Introductory</label>
+                    </div>
+                    <div>
+                        <input type='radio' name='radDifficulty' id='radNormal' value='5'>
+                        <label for='radNormal'>Normal</label>
+                    </div>
+                    <div>
+                        <input type='radio' name='radDifficulty' id='radHeroic' value='6'>
+                        <label for='radHeroic'>Heroic</label>
+                    </div>
                 </div>
 
                 <div class='divInputControl'>
-                    <label for='ddlNumRoles'>Number of Roles:</label>
+                    <label for='ddlNumRoles' class='subtitle'>Number of Roles:</label>
                     <select id='ddlNumRoles'>
                         <option value='2'>2</option>
                         <option value='3'>3</option>
                         <option value='4'>4</option>
                     </select>
-                </div>
-
-                <div class='divInputControl'>
-                    <input type='checkbox' value='random' checked='checked' />
-                    <label for='chkRandomRoles'>Randomize Roles</label>
                 </div>
 
                 <div id='btnPlay' class='button'>PLAY</div>";
