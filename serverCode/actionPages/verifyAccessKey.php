9d1aeea8e2d73d92a1d0ac7ac8dc9697bed98983
Ken H <kenthecoder@outlook.com>
2020-06-18 21:39:51 -0400

    Added server-side logic for verifying access keys.

---
+++ serverCode/actionPages/verifyAccessKey.php
@@ -0,0 +1,51 @@
+<?php
+    try
+    {
+        require "../connect.php";
+        require "../utilities.php";
+
+        $ipAddress = getClientIpAddress();
+        $failedAttemptCount = countFailedAttempts($pdo, $ipAddress);
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data["accessKey"]))
+            throw new Exception("access key not set.");
+
+        $accessKey = $data["accessKey"];
+
+        $stmt = $pdo->prepare("SELECT keyCode, usesRemaining FROM accessKey WHERE keyCode = ?");
+        $stmt->execute([$accessKey]);
+
+        if ($stmt->rowCount() === 0)
+        {
+            recordFailedLoginAttempt($pdo, $accessKey, $ipAddress);
+            throwExceptionIfFailedAttemptLimitReached($failedAttemptCount + 1);
+            throw new Exception("invalid key");
+        }
+        
+        $row = $stmt->fetch();
+
+        if ($row["usesRemaining"] == "0")
+            throw new Exception("key depleted");
+        
+        session_start();
+        $_SESSION["key"] = $accessKey;
+        
+        clearFailedLoginAttempts($pdo);
+        
+        $response["success"] = true;
+    }
+    catch(PDOException $e)
+    {
+        $response["failure"] = "Failed to verify access key: PDOException: " . $e->getMessage();
+    }
+    catch(Exception $e)
+    {
+        $response["failure"] = "Failed to verify access key: " . $e->getMessage();
+    }
+    finally
+    {
+        echo json_encode($response);
+    }
+?>
