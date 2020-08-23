d7f89f431f7156fbc695fde94c08c8f0e70dcecf
Ken H <kenthecoder@outlook.com>
2020-08-23 19:48:57 -0400

    Fixed some minor style issues with the account creation and account verification pages.

--- accountCreation.php
+++ accountCreation.php
@@ -1,25 +1,23 @@
 <?php
-    $html = "<div class='content'>
-                <h1>Account Creation</h1>
-                <p>Choose your username and password.<br/>An email address is required for account verification and password recovery — you will not receive any unwanted emails.</p>
+    $html = "<h1>Account Creation</h1>
+            <p>Choose your username and password.<br/>An email address is required for account verification and password recovery — you will not receive any unwanted emails.</p>
 
-                <form id='frmCreateAccount' autocomplete='off'>
-                    <label for='txtEmailAddress'>Email:</label>
-                    <input id='txtEmailAddress' type='text' />
-                    
-                    <label for='txtUsername'>Username:</label>
-                    <input id='txtUsername' type='text' maxlength='20' />
+            <form id='frmCreateAccount' autocomplete='off'>
+                <label for='txtEmailAddress'>Email:</label>
+                <input id='txtEmailAddress' type='text' />
+                
+                <label for='txtUsername'>Username:</label>
+                <input id='txtUsername' type='text' maxlength='20' />
 
-                    <label for='txtPassword'>Password:</label>
-                    <input id='txtPassword' type='password' autocomplete='new-password' />
+                <label for='txtPassword'>Password:</label>
+                <input id='txtPassword' type='password' autocomplete='new-password' />
 
-                    <label for='txtConfirmPassword'>Confirm Password:</label>
-                    <input id='txtConfirmPassword' type='password' autocomplete='new-password' />
+                <label for='txtConfirmPassword'>Confirm Password:</label>
+                <input id='txtConfirmPassword' type='password' autocomplete='new-password' />
 
-                    <div id='btnCreateAccount' class='button' tabindex='0'>Create Account</div>
-                    <p class='validationError errorSummary hidden'>Please fix the above error(s).</p>
-                </form>
-            </div>";
+                <div id='btnCreateAccount' class='button' tabindex='0'>Create Account</div>
+                <p class='validationError errorSummary hidden'>Please fix the above error(s).</p>
+            </form>";
     
     echo $html;
 ?>
--- accountVerification.php
+++ accountVerification.php
@@ -1,16 +1,14 @@
 <?php
-    $html = "<div class='content'>
-                <h1>Verify Your Account</h1>
-                <p></p>
+    $html = "<h1>Verify Your Account</h1>
+            <p></p>
 
-                <form id='frmVerificationCode' class='inlineForm'>
-                    <label for='txtVerificationCode'>Verification Code:</label>
-                    <input id='txtVerificationCode' type='text' />
-                    <div id='btnVerify' class='button inlineButton'>Verify</div>
-                </form>
+            <form id='frmVerificationCode' class='inlineForm'>
+                <label for='txtVerificationCode'>Verification Code:</label>
+                <input id='txtVerificationCode' type='text' />
+                <div id='btnVerify' class='button inlineButton'>Verify</div>
+            </form>
 
-                <u><a id='lnkResendCode'>Resend Code</a></u>
-            </div>";
+            <u><a id='lnkResendCode'>Resend Code</a></u>";
     
     echo $html;
 ?>
